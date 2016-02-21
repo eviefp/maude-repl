46db57c5aa947be53f94c5e914a39cd9e0593b51
Vlad Ciobanu <admin@cvlad.info>
2016-02-21 15:02:54 +0200

    made it look a bit better

--- public/run.js
+++ public/run.js
@@ -1,6 +1,19 @@
+function toggleHelp() {
+    var shortcuts = document.getElementById('keyboard-shortcuts');
+    if(shortcuts.className == 'show') {
+        shortcuts.className = 'hide';
+    } else {
+        shortcuts.className = 'show';
+    }
+}
+
+function clearTextArea() {
+    document.myCodeMirror.setValue('');
+}
+
 function showResult(event) {
     var output = document.getElementById('output');
-    output.textContent = this.response;
+    output.textContent = `Response at ${new Date().toLocaleTimeString()}\n` + this.response;
 }
 
 function sendCommand() {
@@ -16,5 +29,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var button = document.getElementById('run');
     var input = document.getElementById('input');
     button.onclick = sendCommand;
-    document.myCodeMirror = CodeMirror.fromTextArea(input);    
+    document.myCodeMirror = CodeMirror.fromTextArea(input, {
+        theme: "zenburn",
+        lineNumbers: true,
+        autofocus: true,
+        viewportMargin: 100
+    });
+    document.myCodeMirror.setOption("extraKeys", {
+       F9: sendCommand,
+       'Ctrl-/': toggleHelp,
+       F2: clearTextArea
+    });
 });
